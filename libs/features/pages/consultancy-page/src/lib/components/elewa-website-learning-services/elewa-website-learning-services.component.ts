8e2d31e10e2dea0bdf595238a3971b5229fde434
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-09-07 10:49:45 +0300

    1. ✨feat: Create the elewa-website-learning-services component.
    2. ✨feat: Populate the main Consultancy Page Component with the InfoCard data.
    
    3. ✨feat: Use input to implement the InfoCard data through the cardsData declaration from the step above.
    
    4. ✨feat: call the created component in the main consultancy oage component html file.

---
+++ libs/features/pages/consultancy-page/src/lib/components/elewa-website-learning-services/elewa-website-learning-services.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
+
+@Component({
+  selector: 'elewa-website-elewa-website-learning-services',
+  templateUrl: './elewa-website-learning-services.component.html',
+  styleUrls: ['./elewa-website-learning-services.component.scss'],
+})
+export class ElewaWebsiteLearningServicesComponent {
+  @Input() cardsData: InfoCard[] = [];
+}
